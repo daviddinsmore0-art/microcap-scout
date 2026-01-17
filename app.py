16b981f916ed8bb990d6c2ba6f6d306777793c44
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-01-17 15:39:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -294,6 +294,8 @@ def display_ticker_grid(ticker_list, live_mode=False):
 def fetch_rss_items():
     headers = {'User-Agent': 'Mozilla/5.0'}
     urls = [
+        "https://rss.app/feeds/T1dwxaFTbqidPRNW.xml",
+        "https://rss.app/feeds/T1dwxaFTbqidPRNW.xml",
         "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=15839069",
         "https://feeds.content.dowjones.io/public/rss/mw_topstories"
     ]
