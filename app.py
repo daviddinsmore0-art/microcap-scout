b2756791fe502257cac7b1f6ca5e30bb866b7284
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-01-18 21:20:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -279,7 +279,7 @@ def get_news_cached():
         try:
             r = requests.get(u, headers=head, timeout=5)
             root = ET.fromstring(r.content)
-            for i in root.findall('.//item')[:5]:
+            for i in root.findall('.//item')[:50]:
                 t, l = i.find('title').text, i.find('link').text
                 if t and t not in seen:
                     t_lower = t.lower()
