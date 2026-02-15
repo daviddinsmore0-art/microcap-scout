58f380ac0446f8bd6e4c56f7177d82978f6d7083
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-15 13:56:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1673,10 +1673,10 @@ if tab == "home":
               <div style='letter-spacing:2px; font-weight:800; color:#57b3ff; margin-bottom:10px;'>
                 BIG MOVERS (Top 3)
               </div>
-              <div style='font-size:22px; font-weight:900; margin-bottom:8px;'>📈 GAINERS</div>
+              <div style='font-size:20px; font-weight:900; margin-bottom:8px;'>📈 GAINERS</div>
               {gainers_html}
               <div style='height:10px'></div>
-              <div style='font-size:22px; font-weight:900; margin-bottom:8px;'>📉 LOSERS</div>
+              <div style='font-size:20px; font-weight:900; margin-bottom:8px;'>📉 LOSERS</div>
               {losers_html}
             </div>
             """,
