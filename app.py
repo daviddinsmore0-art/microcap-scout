b729ef02ea8013b30732ec50abea40755b374744
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-14 22:21:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -639,9 +639,8 @@ div[data-testid="stToolbar"] { display: none !important; }
 }
 
 .pplogo{
-  height:26px;           /* bump to 24–28 if you want bigger */
+  height:24px;           /* bump to 24–28 if you want bigger */
   width:auto;
-  max-width: 80px;
   display:block;
   object-fit:contain;
 }
