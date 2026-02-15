fa6d78375a150283f6bfb55b379af7cd1850940c
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-15 14:53:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ div[data-testid="stDecoration"] {
     font-weight: 300;
     color: #A7F3D0;
     letter-spacing: 0.5px;
-    margin: 10px 0 30px 0;
+    margin: 10px 0 20px 0;
 }
         /* Metric Boxes */
         .metric-box {
