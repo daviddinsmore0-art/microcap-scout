66edb1a62ea43e5fb8fd0477d8cdcff6ff915454
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-14 22:30:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,9 @@ footer {visibility: hidden;}
             padding: 10px;
         }
         div[data-baseweb="input"] { background-color: transparent !important; border: none; }
-        
+        .block-container{
+    padding-top: 0.25rem !important;
+}
         /* Dropdowns & Select Boxes (FIXED) */
         div[data-baseweb="select"] > div { 
             background-color: #1e293b !important; 
