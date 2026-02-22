ce944fc62113ca96f5b7e6a9869cfb6e36a9cf68
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-22 14:08:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2231,9 +2231,9 @@ if tab == "home":
                 pass
 
             accel_html = "<br>".join(bullets)
-
+            import textwrap
             st.markdown(
-                f"""
+               st.markdown(textwrap.dedent f"""
 <div style='margin-top:18px; border-radius:18px; overflow:hidden; background:linear-gradient(145deg,#0f172a,#0b1220); box-shadow:0 10px 30px rgba(0,0,0,0.4); border:1px solid rgba(255,255,255,0.05);'>
 
   <div style='display:flex;'>
