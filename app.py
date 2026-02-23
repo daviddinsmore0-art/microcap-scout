d0cd45d257c53c96782310990f013b223cec3fa3
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-22 23:06:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2336,6 +2336,7 @@ if tab == "home":
 
   <div style="margin-top:18px; text-align:right; font-size:.85rem; letter-spacing:1px; opacity:.8;">
   </div>
+</div>
 <div style="
     height: 1px; 
     width: 80%; 
@@ -2343,8 +2344,6 @@ if tab == "home":
     background: linear-gradient(90deg, transparent, #facc15, transparent);
     box-shadow: 0px -1px 8px rgba(250, 204, 21, 0.5);
 "></div>
-
-</div>
 """).strip()
 
             st.markdown(card_html, unsafe_allow_html=True)
