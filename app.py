81a2b30971280bfe8a23308e2aac8094aba60265
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-23 09:48:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2307,7 +2307,7 @@ if tab == "home":
     margin-top:20px;
     margin-bottom:20px;
     border-radius:0px 30px 30px 0px;
-    padding:5px 16px 22px 16px;
+    padding:0px 0px 22px 0px;
     background:linear-gradient(145deg,#0f172a,#0b1220);
     box-shadow:0 12px 35px rgba(0,0,0,0.45);
     border:1px solid rgba(255,255,255,0.06);
