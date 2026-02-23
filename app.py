e13bdcee3dc7d6e2cdacb6b72b7944f5e33689f5
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-23 09:50:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2307,7 +2307,7 @@ if tab == "home":
     margin-top:20px;
     margin-bottom:20px;
     border-radius:0px 30px 30px 0px;
-    padding:0px 0px 22px 0px;
+    padding:0px 0px 0px 0px;
     background:linear-gradient(145deg,#0f172a,#0b1220);
     box-shadow:0 12px 35px rgba(0,0,0,0.45);
     border:1px solid rgba(255,255,255,0.06);
@@ -2317,7 +2317,7 @@ if tab == "home":
 
 
   <!-- TITLE ROW -->
-  <div style="display:flex; justify-content:space-between; align-items:center; margin-top:2px; background:#0b0000;">
+  <div style="display:flex; justify-content:space-between; align-items:center; margin-top:0px; height:80px; background:#0b0000;">
     <div style="font-size:1rem; font-weight:600; color:#cbd5e1;">
       Today's <span style="color:white;">Signal Shift</span>
     </div>
