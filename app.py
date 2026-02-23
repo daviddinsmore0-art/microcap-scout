19abdee99fc980f1a1f4061f7f74f7d5b64e74a9
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-23 14:28:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2329,7 +2329,7 @@ if tab == "home":
             card_html = textwrap.dedent(f"""
 <div style="
     margin-top:40px;
-    margin-bottom:10px;
+    margin-bottom:0px;
     border-radius:20px;
     padding:10px 10px 20px 20px;
     background:linear-gradient(145deg,#0f172a,#0b1220);
@@ -2553,7 +2553,7 @@ if tab == "home":
 
         card_html = textwrap.dedent(f"""
         <div style="
-            margin-top:10px;
+            margin-top:0px;
             margin-bottom:10px;
             border-radius:20px;
             padding:10px 10px 20px 20px;
