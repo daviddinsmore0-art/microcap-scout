4dbcb75eb27b311ceb0357f0d8074b2e61d94dce
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-23 14:50:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2553,7 +2553,7 @@ if tab == "home":
 
         card_html = textwrap.dedent(f"""
         <div style="
-            margin-top:0px;
+            margin-top:-10px;
             margin-bottom:0px;
             border-radius:20px;
             padding:10px 10px 20px 20px;
