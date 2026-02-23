2a1c02c9e987993e8e04244d95362869417606aa
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-23 10:37:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2334,14 +2334,7 @@ if tab == "home":
   {f'<div style="margin-top:12px; color:#cbd5e1; line-height:1.6;">{accel_html}</div>' if accel_html else ''}
 
 </div>
-<div style="
-    height: 1px; 
-    width: 80%;
-    margin-top:-50px;
-    margin: 0 auto; 
-    background: linear-gradient(90deg, transparent, #facc15, transparent);
-    box-shadow: 0px -1px 8px rgba(250, 204, 21, 0.5);
-"></div>
+
 """).strip()
 
             st.markdown(card_html, unsafe_allow_html=True)
