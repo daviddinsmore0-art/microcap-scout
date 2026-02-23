298f2518080ede276bfb8503b4720bf8d587c63d
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-23 12:41:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2320,7 +2320,7 @@ if tab == "home":
   <div style="display:flex; justify-content:space-between; align-items:center; margin-top:0px; padding-bottom:5px; border-bottom: 1px solid #2d3748;">
    <div style="font-size:1rem; font-weight:600; color:#cbd5e1;">
      Today's <span style="color:white;">Signal Shift</span>
-    </div>
+    
   </div>
     <div style="color:#22c55e; font-weight:900;"></div>
   </div>
