a786b99870013c7bdebac69859b34f1e4733e2a0
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-26 11:38:46 -0400

    Add gradient line with shadow to radar card

--- app.py
+++ app.py
@@ -2913,6 +2913,15 @@ if tab == "home":
        </div>
        </div>
        </div>
+       <div style="
+                position: absolute;
+                bottom: 0;
+                left: 10%;
+                width: 80%;
+                height: 1px;
+                background: linear-gradient(90deg, transparent, #4ade80, transparent);
+                box-shadow: 0px -2px 10px rgba(74, 222, 128, 0.6);
+              "></div>
        </div>
     """
 
