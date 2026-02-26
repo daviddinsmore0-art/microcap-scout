e17c7542795e57414206a3eb7fd55bd4338978af
daviddinsmore0-art <david.dinsmore0@gmail.com>
2026-02-26 16:44:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2996,7 +2996,7 @@ if tab == "home":
         peak_focus_score,
         peak_day_change,
         peak_candle_ts,
-        TIMESTAMPDIFF(MINUTE, last_seen_ts, NOW()) AS minutes_ago
+        TIMESTAMPDIFF(MINUTE, peak_candle_ts, NOW()) AS minutes_ago
         FROM breakout_radar_daily
         WHERE trade_date = CURDATE()
         AND peak_dir = 'bear'
